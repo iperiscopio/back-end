9d5a1817b84d7660b21686f90df48de37ef688ee
iperiscopio <49154268+iperiscopio@users.noreply.github.com>
2021-12-18 17:58:58 +0000

    restructure of data from projects controller to projectManager controller and creation of authentication through JWT

--- api/controllers/projectManager.php
+++ api/controllers/projectManager.php
@@ -1,9 +1,126 @@
 <?php
 
+    use ReallySimpleJWT\Token;
+
+    require("models/config.php");
     require("models/project.php");
-    require("models/user.php");
+
+    $config = new Config();
 
     $model = new Project();
+
+    // User authentication through JWT
+    if( in_array($_SERVER["REQUEST_METHOD"], ["GET","POST", "PUT", "DELETE"]) ) {
+        
+        $userId = $config->routeRequireValidation();
+        var_dump($userId);
+
+        if( empty( $userId ) ) {
+            http_response_code(401);
+            return '{"message":"Wrong or missing Auth Token"}';
+        } 
+
+        if( 
+            !empty($id)
+        ) {
+            http_response_code(403);
+            die('{"message":"You do not have permission to perform this action"}');
+        }
+
+    }
+     
+
+     // Sanitize:
+     function sanitize($data) {
+        if( !empty($data) ) {
+            $data["title"] = trim(htmlspecialchars(strip_tags($data["title"]))); 
+            $data["location"] = trim(htmlspecialchars(strip_tags($data["location"]))); 
+            $data["description"] = trim(htmlspecialchars(strip_tags($data["description"])));
+
+            for( $i = 0; $i < count($data["images"]); $i++ ) {
+
+                $sanitize = trim(htmlspecialchars(strip_tags($data["images"][$i])));
+                $data["images"][$i] = str_replace("data:image/jpeg;base64,", "", $sanitize);
+            } 
+
+            return $data;
+        }
+
+        return false;
+    }
+
+    // Validate:
+    function validator($sanitizedData) {
+
+        if( !empty($sanitizedData) ) {
+
+            for( $i = 0; $i < count($sanitizedData["images"]); $i++ ) {
+
+                $size = strlen($sanitizedData["images"][$i]);
+
+                if( 
+                    isset($sanitizedData["title"]) &&
+                    isset($sanitizedData["location"]) &&
+                    isset($sanitizedData["description"]) &&
+                    mb_strlen($sanitizedData["title"]) >= 3 &&
+                    mb_strlen($sanitizedData["title"]) <= 250 &&
+                    mb_strlen($sanitizedData["location"]) >= 3 &&
+                    mb_strlen($sanitizedData["location"]) <= 120 &&
+                    mb_strlen($sanitizedData["description"]) >= 3 &&
+                    mb_strlen($sanitizedData["description"]) <= 10000 &&
+                    $size > 0 &&
+                    $size < 10000000
+                ) {
+                
+                    return true;
+                } 
+                
+            }
+            
+        }   
+
+        return false;
+
+    }
+
+    // Transform images data:
+    function imageTransformation($sanitizedData) {
+        
+        $target_dir = "/images/";
+        
+        // allowed image formats array
+        $allowed_files_formats = [
+            "jpg" => "image/jpeg",
+            "png" => "image/png",
+            "gif" => "image/gif",
+            "webp" => "image/webp",
+            "svg" => "image/svg+xml"
+        ];
+
+        for( $i = 0; $i < count($sanitizedData["images"]); $i++ ) {
+
+            $decoded_image = base64_decode($sanitizedData["images"][$i]);
+
+            $finfo = new finfo(FILEINFO_MIME_TYPE);
+
+            $detected_format = $finfo->buffer($decoded_image);
+
+            if(in_array($detected_format, $allowed_files_formats)) {
+
+                $filename = str_replace(" ", "_", $sanitizedData["title"]) . "_" . bin2hex(random_bytes(4));
+                
+                $extension = "." . array_search($detected_format, $allowed_files_formats);
+
+                $file_dir = $target_dir . $filename . $extension;
+
+                file_put_contents(".." . $file_dir, $decoded_image);
+                
+            }
+            $sanitizedData["images"][$i] = $file_dir;
+        }
+        
+        return $sanitizedData;
+    }
 
 
     if($_SERVER["REQUEST_METHOD"] === "GET") {
@@ -12,7 +129,93 @@
         echo json_encode( $model->getAllProjects() );
 
 
-    } elseif($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    }  elseif($_SERVER["REQUEST_METHOD"] === "POST") { 
+
+        $data = json_decode( file_get_contents("php://input"), TRUE );
+
+        $sanitizedData = sanitize($data);
+        $transformedData = imageTransformation($sanitizedData);
+        
+        if( validator($sanitizedData) ) {
+            
+            $model->createProject( $transformedData );
+    
+            http_response_code(202);
+            die('{"message": "Uploaded project ' . $transformedData["title"] . ' with success"}');
+
+        } else {
+
+            http_response_code(400);
+            die('{"message": "400 Bad Request"}');
+        }
+
+
+
+        
+    } else if($_SERVER["REQUEST_METHOD"] === "PUT") { 
+
+        $data = json_decode( file_get_contents("php://input"), TRUE );
+        
+        $sanitizedData = sanitize($data);
+        $transformedData = imageTransformation($sanitizedData);
+
+        if( 
+            !empty($id) &&
+            validator($sanitizedData)
+        ) {
+            $updateProject = $model->updateProject( $id, $transformedData );
+            
+            if( $updateProject ) {
+                http_response_code(202);
+
+                echo json_encode( $updateProject );
+
+                die('{"message": "Updated project ' . $id . ', ' . $transformedData["title"] . ' with success"}');
+
+            } else {
+                http_response_code(404);
+                die('{"message": "404 Not Found"}');
+            }
+            
+            
+    
+        } else {
+            http_response_code(400);
+            die('{"message": "400 Bad Request"}');
+        }
+
+
+
+
+    } else if($_SERVER["REQUEST_METHOD"] === "DELETE") { 
+
+        $data = json_decode( file_get_contents("php://input"), TRUE );
+        
+        if( !empty( $id ) && is_numeric( $id ) ) {
+
+            $removeProject = $model->deleteProject($id);
+            
+            if( $removeProject ) { 
+
+                http_response_code(202);
+                die('{"message": "Deleted Project nº: ' . $id . ' ' . $data["title"] .'"}');
+                
+            } else {
+
+                http_response_code(404);
+                die('{"message": "404 Not Found"}');
+
+            }
+            
+        } else {
+
+             http_response_code(400);
+            die('{"message": "400 Bad Request"}');
+
+        }
+
+        
+    }elseif($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
         http_response_code(202);
 
     }else {
